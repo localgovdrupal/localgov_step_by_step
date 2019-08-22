6cf256c7588c60a500903a14c6de195b121372e6
Adnan <adnan.muhammad@croydon.gov.uk>
2019-08-22 12:37:28 +0100

    Javascript and CSS placeholder files for Step by step.

---
+++ js/step-by-step-nav.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Additional behaviour for the Step by step navigation.
+ */
+
+(function($, Drupal, drupalSettings) {
+
+  Drupal.behaviors.step_by_step = {
+    attach: function onload(context, settings) {
+
+      // Custom code goes here.
+    }
+  }
+})(jQuery, Drupal, drupalSettings);
