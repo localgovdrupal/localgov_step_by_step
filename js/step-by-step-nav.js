919d831dd7b0beb260ce5b4134c8aa5e05969bfe
Adnan <adnan.muhammad@croydon.gov.uk>
2019-08-22 15:31:34 +0100

    Update for Step by step module's default config.
    We now provide a Step by step navigation block for the Step by step homepage.

--- js/step-by-step-nav.js
+++ js/step-by-step-nav.js
@@ -8,7 +8,8 @@
   Drupal.behaviors.step_by_step = {
     attach: function onload(context, settings) {
 
-      // Custom code goes here.
+      // Custom code goes here.  Please note that this function can get called
+      // more than once after page load.
     }
   }
 })(jQuery, Drupal, drupalSettings);
