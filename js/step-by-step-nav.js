c2fea6d33d19725d3dce03f7fd8b26f669177b7d
chris stevens <cjs78@hotmail.co.uk>
2019-09-03 10:38:31 +0100

    Step by Step styling

--- js/step-by-step-nav.js
+++ js/step-by-step-nav.js
@@ -7,9 +7,20 @@
 
   Drupal.behaviors.step_by_step = {
     attach: function onload(context, settings) {
-
       // Custom code goes here.  Please note that this function can get called
       // more than once after page load.
+      console.log('working');
+
+
+      $(".step-list .step").each(function(i) {
+	      if ($(this).hasClass('step--active')) {
+	      	this.append("<a href=''>Show Less</a>");
+	      } else {
+	      	this.append("<a href=''>Show More</a>");
+	      };
+	  });
+      
+
     }
   }
 })(jQuery, Drupal, drupalSettings);
