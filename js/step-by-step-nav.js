209f1a01f3ac32b3e07b2912e98e5796307e31b8
chris stevens <cjs78@hotmail.co.uk>
2019-09-06 13:55:07 +0100

    commit prior to config merge - changes relating to step by step

--- js/step-by-step-nav.js
+++ js/step-by-step-nav.js
@@ -4,21 +4,70 @@
  */
 
 (function($, Drupal, drupalSettings) {
+  console.log('running');
+  function titlesum(){ 
+    // add initial step view links based on active step status
+    $("ol.step-list .step").each(function() {
+        if ($(this).hasClass('step--active')) {
+            $(this).find('.step__title').append("<a href='#' class='step-show'>Show less</a>");
+            $(this).find('.step__summary').addClass('step-show-summary');
+        } else {
+            $(this).find('.step__title').append("<a href='#' class='step-show'>Show more</a>");
+        };
+     });
+    }
+
+  function stepstext(){
+    $("ol.step-list .step").each(function() {
+      if ($(".step__summary").is(':visible')) {
+         $(this).find('.step-show').text('Show less');
+      } else {
+         $(this).find('.step-show').text('Show more');
+      }
+    });
+
+  }
+
+  // run titlesum funtion on load
+  titlesum();
+
+  // add the hide all option
+  $("<i class='fas fa-eye'></i><a href='#' class='step-master pl-2'>Hide steps</a>").insertBefore("ol.step-list");
+  
+
+  // handle show hide of summaries
+  $('.step-show').on("click", function (e){
+
+    $(this).parent().siblings(".step__summary").toggleClass("step-show-summary");
+
+    if ($(this).parent().siblings(".step__summary").is(':visible')) {
+             $(this).text('Show less');                
+        } else {
+             $(this).text('Show more');                
+        }
+    e.preventDefault();
+    
+  });
+
+  // handle hide show of all summaries
+  $('.step-master').on("click", function (e){
+
+    if ($(".step__summary").is(':visible')) {
+      $(".step__summary").removeClass("step-show-summary");
+            $(this).text('Show steps');             
+        } else {
+          $(".step__summary").addClass("step-show-summary");
+            $(this).text('Hide steps');            
+        }
+    stepstext(); // update the steps text
+    e.preventDefault();
+  });
+  
 
   Drupal.behaviors.step_by_step = {
     attach: function onload(context, settings) {
       // Custom code goes here.  Please note that this function can get called
       // more than once after page load.
-      console.log('working');
-
-
-      $(".step-list .step").each(function(i) {
-	      if ($(this).hasClass('step--active')) {
-	      	this.append("<a href=''>Show Less</a>");
-	      } else {
-	      	this.append("<a href=''>Show More</a>");
-	      };
-	  });
       
 
     }
