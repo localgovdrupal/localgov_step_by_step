559d4b05b25b68f25d26ea246d9dc038567979f2
Finn <finn@finnlewis.com>
2020-05-12 15:52:57 +0100

    Update from Adnan.

--- js/step-by-step-nav.js
+++ js/step-by-step-nav.js
@@ -9,10 +9,10 @@
   function titlesum(){ 
     $("ol.step-list .step").each(function() {
         if ($(this).hasClass('step--active')) {
-            $(this).find('.step__title').append("<a href='#' class='step-show'>Show less about this step</a>");
+            $(this).find('.step__title').append("<span class='step-summary-container'><a href='#' class='step-show'>Hide step summary</a></span>");
             $(this).find('.step__summary').addClass('step-show-summary');
         } else {
-            $(this).find('.step__title').append("<a href='#' class='step-show'>Show more about this step</a>");
+            $(this).find('.step__title').append("<span class='step-summary-container'><a href='#' class='step-show'>Show step summary</a></span>");
         };
      });
     }
@@ -21,9 +21,9 @@
   function stepstext(){
     $("ol.step-list .step").each(function() {
       if ($(".step__summary").is(':visible')) {
-         $(this).find('.step-show').text('Show less about this step');
+         $(this).find('.step-show').text('Hide step summary');
       } else {
-         $(this).find('.step-show').text('Show more about this step');
+         $(this).find('.step-show').text('Show step summary');
       }
     });
   }
@@ -33,18 +33,18 @@
 
   // add the hide all option on load
   if ($(".step__summary").is(':visible')) {
-  $("<i class='fas fa-eye-slash'></i><a href='#' class='step-master pl-2'>Hide steps</a>").insertBefore("ol.step-list");
+  $("<div class='summaries-control'><i class='fas fa-eye-slash'></i><a href='#' class='step-master ml-2'>Hide summaries</a></div>").insertBefore("ol.step-list");
   } else {
-    $("<i class='fas fa-eye'></i><a href='#' class='step-master pl-2'>Show steps</a>").insertBefore("ol.step-list");
+    $("<div class='summaries-control'><i class='fas fa-eye'></i><a href='#' class='step-master ml-2'>Show summaries</a></div>").insertBefore("ol.step-list");
   };
 
   // handle show hide of summaries
   $('.step-show').on("click", function (e){
-    $(this).parent().siblings(".step__summary").toggleClass("step-show-summary");
-    if ($(this).parent().siblings(".step__summary").is(':visible')) {
-      $(this).text('Show less about this step');                
+    $(this).parent().parent().siblings(".step__summary").toggleClass("step-show-summary");
+    if ($(this).parent().parent().siblings(".step__summary").is(':visible')) {
+      $(this).text('Hide step summary');                
     } else {
-      $(this).text('Show more about this step');                
+      $(this).text('Show step summary');                
     }
     e.preventDefault();
   });
@@ -53,11 +53,11 @@
   $('.step-master').on("click", function (e){
    if ($(".step__summary").is(':visible')) {
     $(".step__summary").removeClass("step-show-summary");
-      $(this).text('Show steps');
+      $(this).text('Show summaries');
       $(this).prev().addClass('fa-eye').removeClass('fa-eye-slash');         
     } else {
       $(".step__summary").addClass("step-show-summary");
-      $(this).text('Hide steps'); 
+      $(this).text('Hide summaries'); 
       $(this).prev().addClass('fa-eye-slash').removeClass('fa-eye');          
     }
     
