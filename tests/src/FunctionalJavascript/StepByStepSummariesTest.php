572631e55ebaa6619540ee29ac8d9d82d3233c11
Dan Champion <support@championinternet.com>
2020-07-17 08:58:55 +0100

    Step summary visiblity javascript tests

--- tests/src/FunctionalJavascript/StepByStepSummariesTest.php
+++ tests/src/FunctionalJavascript/StepByStepSummariesTest.php
@@ -3,9 +3,10 @@
 namespace Drupal\Tests\localgov_alert_banner\FunctionalJavascript;
 
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+use Drupal\node\NodeInterface;
 
 /**
- * Functional tests for LocalGovDrupal Alert banner block.
+ * Javascript tests for LocalGovDrupal step-by-step blocks.
  */
 class StepByStepSummariesTest extends WebDriverTestBase {
 
@@ -20,56 +21,65 @@ class StepByStepSummariesTest extends WebDriverTestBase {
   protected $profile = 'localgov';
 
   /**
-   * {@inheritdoc}
+   * Modules to enable.
+   *
+   * @var array
    */
   public static $modules = [
-    'localgov_alert_banner',
+    'localgov_step_by_step',
   ];
 
   /**
-   * Test alert banner hide link.
+   * Test step summary visibility
    */
-  public function testAlertBannerHide() {
-    // Set up an alert banner.
-    $title = $this->randomMachineName(8);
-    $alert_message = 'Alert message: ' . $this->randomMachineName(16);
-    $alert = $this->container->get('entity_type.manager')->getStorage('localgov_alert_banner')
-      ->create([
-        'type' => 'localgov_alert_banner',
-        'title' => $title,
-        'short_description' => $alert_message,
-        'type_of_alert' => 'minor',
-        'status' => TRUE,
+  public function testStepSummaryVisibility() {
+
+    // Create step-by-step overview node.
+    $overview_title = $this->randomMachineName(8);
+    $overview = $this->createNode([
+      'title' => $overview_title,
+      'type' => 'localgov_step_by_step_overview',
+      'status' => NodeInterface::PUBLISHED,
+    ]);
+
+    // Create three step-by-step page nodes.
+    $pages = array();
+    for ($x = 1; $x < 4; $x++) {
+      $pages[$x] = $this->createNode([
+        'title' => 'Step ' . $x . ' page',
+        'localgov_step_section_title' => 'Step ' . $x . ' title',
+        'type' => 'localgov_step_by_step_page',
+        'localgov_step_summary' => 'Step ' . $x . ' summary.',
+        'status' => NodeInterface::PUBLISHED,
+        'localgov_step_parent' => ['target_id' => $overview->id()],
       ]);
-    $alert->save();
+    }
 
-    // Load the front page.
-    $this->drupalGet('<front>');
+    // Load overview page.
+    $this->drupalGet('/node/1');
 
-    // Find and click hide link.
+    //Check summaries not visible.
+    $this->assertSession()->pageTextNotContains('Step 1 summary');
+
     $page = $this->getSession()->getPage();
-    $button = $page->findButton('Hide');
-    $this->assertNotEmpty($button);
-    $button->click();
 
-    // Check cookie set and banner not visible.
-    $this->assertSession()->CookieExists('hide-alert-banner-token');
-    $this->assertSession()->pageTextNotContains($alert_message);
+    // Test 'Show summaries' button.
+    $page->pressButton('Show summaries');
+    $this->assertSession()->pageTextContains('Step 1 summary');
+    $this->assertSession()->pageTextContains('Step 2 summary');
+    $this->assertSession()->pageTextContains('Step 3 summary');
 
-    // Test on login page.
-    $this->drupalGet('/user');
-    $this->assertSession()->pageTextNotContains($alert_message);
+    // Test 'Hide summaries' button.
+    $page->pressButton('Hide summaries');
+    $this->assertSession()->pageTextNotContains('Step 1 summary');
+    $this->assertSession()->pageTextNotContains('Step 2 summary');
+    $this->assertSession()->pageTextNotContains('Step 3 summary');
 
-    // Update alert message.
-    $title = $this->randomMachineName(8);
-    $alert->set('title', ['value' => $title]);
-    $alert->save();
-
-    // Load the front page and check that banner displays and cookie token is
-    // no longer valid.
-    $this->drupalGet('<front>');
-    $this->assertSession()->pageTextContains($title);
-
+    // Load step 2 page and test summary visibility.
+    $this->drupalGet('/node/3');
+    $this->assertSession()->pageTextNotContains('Step 1 summary');
+    $this->assertSession()->pageTextNotContains('Step 3 summary');
+    $this->assertSession()->pageTextContains('Step 2 summary');
   }
 
 }
