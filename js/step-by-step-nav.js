8584d4e733ba38bd0cf7e3169cec31a74c96bf72
Dan Champion <support@championinternet.com>
2020-07-17 06:41:03 +0100

    Coding standards

--- js/step-by-step-nav.js
+++ js/step-by-step-nav.js
@@ -11,7 +11,7 @@
   stepByStep.showStepText = 'Show step summary';
   stepByStep.hideStepText = 'Hide step summary';
 
-  // Set visibility based on specified button.step-show elements
+  // Set visibility based on specified button.step-show elements.
   function summaryVisiblity(elements, cmd) {
     switch(cmd) {
       case 'show':
@@ -22,12 +22,13 @@
           $(this).attr("aria-expanded", "true");
           $(this).attr('aria-label', "Hide " + stepTitle + " summary");
         });
-        // 'Hide all' control displayed if all steps are shown
+        // 'Hide all' control displayed if all steps are shown.
         if ($('.step__summary').length === $('.step-show-summary').length) {
           $('.step-master').text(stepByStep.hideAllText);
           $('.summaries-control i').addClass('fa-eye-slash').removeClass('fa-eye');
         }
         break;
+
       case 'hide':
         elements.each(function() {
           var stepTitle = $(this).parents('.step__title').find('a').text();
@@ -36,25 +37,25 @@
           $(this).text(stepByStep.showStepText);
           $(this).attr('aria-label', "Show " + stepTitle + " summary");
         });
-        // 'Show all' control displayed if any steps are hidden
+        // 'Show all' control displayed if any steps are hidden.
         $('.step-master').text(stepByStep.showAllText);
         $('.summaries-control i').addClass('fa-eye').removeClass('fa-eye-slash');
         break;
     }
   }
 
-  // Insert show all button
+  // Insert show all button.
   $("<div class='summaries-control'><i class='fas fa-eye'></i><button aria-expanded='false' class='step-master ml-2'>" + stepByStep.showAllText + "</button></div>").insertBefore("ol.step-list");
 
-  // Insert hide/show button for each step
+  // Insert hide/show button for each step.
   function stepSummaryButton(isVisible, stepTitle) {
-    var container = $("<span class='step-summary-container'>");
-    var button = $("<button class='step-show'>");
-    button.attr('aria-expanded', isVisible ? "true" : "false");
-    button.attr('aria-label', (isVisible ? "Hide " : "Show ") + stepTitle + " summary");
-    button.text(isVisible ? stepByStep.hideStepText : stepByStep.showStepText);
-    container.append(button);
-    return container;
+    var $container = $("<span class='step-summary-container'>");
+    var $button = $("<button class='step-show'>");
+    $button.attr('aria-expanded', isVisible ? "true" : "false");
+    $button.attr('aria-label', (isVisible ? "Hide " : "Show ") + stepTitle + " summary");
+    $button.text(isVisible ? stepByStep.hideStepText : stepByStep.showStepText);
+    $container.append($button);
+    return $container;
   }
 
   $("ol.step-list .step").each(function() {
@@ -66,7 +67,7 @@
     $(this).find('.step__title').append(stepSummaryButton(isVisible, stepTitle));
   });
 
-  // Show / hide all
+  // Show / hide all.
   $('.step-master').on("click", function () {
     $('.summaries-control i').toggleClass('fa-eye fa-eye-slash');
     if ($(this).text() === stepByStep.showAllText) {
@@ -78,7 +79,7 @@
     }
   });
 
-  // Show / hide single step
+  // Show / hide single step.
   $('.step-show').on("click", function () {
     $(this).parents('.step').find('.step__summary').toggleClass('step-show-summary');
     if ($(this).text() === stepByStep.showStepText) {
