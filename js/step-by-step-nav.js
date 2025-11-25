21a9a2d69bd377270c00c59d8d9e3dafe4ab4764
Christopher Torgalson <manager@bedlamhotel.com>
2025-11-25 08:49:54 -0500

    fix: replaces prettier's double quotes with single per eslint

--- js/step-by-step-nav.js
+++ js/step-by-step-nav.js
@@ -25,9 +25,9 @@
         steps.filter((step) => step.button.getAttribute(stepAriaAttr) !== attrValue)
 
         if (expanded) {
-          message = Drupal.t("Step summaries expanded");
+          message = Drupal.t('Step summaries expanded');
         } else {
-          message = Drupal.t("Step summaries collapsed");
+          message = Drupal.t('Step summaries collapsed');
         }
 
         Drupal.announce(message);
@@ -73,7 +73,7 @@
           return;
         }
 
-        const pressed = target.getAttribute(stepControlStateAttr) !== "true";
+        const pressed = target.getAttribute(stepControlStateAttr) !== 'true';
         toggleControlButton(pressed);
         toggleAllSummaries(pressed);
       }
@@ -89,19 +89,19 @@
         let ariaLabel;
 
         if (expanded) {
-          ariaLabel = Drupal.t("Hide step summary - !stepTitle", {
-            "!stepTitle": title,
+          ariaLabel = Drupal.t('Hide step summary - !stepTitle', {
+            '!stepTitle': title,
           });
         } else {
-          ariaLabel = Drupal.t("Show step summary - !stepTitle", {
-            "!stepTitle": title,
+          ariaLabel = Drupal.t('Show step summary - !stepTitle', {
+            '!stepTitle': title,
           });
         }
 
-        button.innerHTML = Drupal.theme("stepButtonText", expanded);
+        button.innerHTML = Drupal.theme('stepButtonText', expanded);
         button.setAttribute(stepAriaAttr, expanded);
-        button.setAttribute("aria-label", ariaLabel);
-        summary.classList[expanded ? "add" : "remove"]("step-show-summary");
+        button.setAttribute('aria-label', ariaLabel);
+        summary.classList[expanded ? 'add' : 'remove']('step-show-summary');
       }
 
       /**
@@ -117,11 +117,11 @@
 
         toggleStepButton(
           steps[index],
-          target.getAttribute(stepAriaAttr) !== "true",
+          target.getAttribute(stepAriaAttr) !== 'true',
         );
 
         const hiddenSteps = !!steps.some(
-          (step) => step.button.getAttribute(stepAriaAttr) === "false",
+          (step) => step.button.getAttribute(stepAriaAttr) === 'false',
         );
 
         // 'Show all' control displayed if any steps are hidden, and 'Hide all'
@@ -148,31 +148,31 @@
       /**
        * Set up global variables.
        */
-      const [stepListEl] = once("sts-steplist", "ol.step-list", context);
+      const [stepListEl] = once('sts-steplist', 'ol.step-list', context);
 
       if (!stepListEl) {
         return;
       }
 
-      const stepEls = once("sts-step", ".step", stepListEl);
-      const stepAriaAttr = "aria-expanded";
-      const stepControlStateAttr = "data-pressed";
+      const stepEls = once('sts-step', '.step', stepListEl);
+      const stepAriaAttr = 'aria-expanded';
+      const stepControlStateAttr = 'data-pressed';
       const steps = [];
       const stepIds = [];
 
       /**
        * Set up step buttons.
        */
-      const stepButtonTemplate = document.createElement("template");
-      stepButtonTemplate.innerHTML = Drupal.theme("stepButtonHtml");
+      const stepButtonTemplate = document.createElement('template');
+      stepButtonTemplate.innerHTML = Drupal.theme('stepButtonHtml');
       stepEls.forEach((stepEl, index) => {
         const stepId = getStepId();
         const buttonMarkup = stepButtonTemplate.content.cloneNode(true);
-        const stepTitleEl = stepEl.querySelector(".step__title");
+        const stepTitleEl = stepEl.querySelector('.step__title');
         const step = {
-          button: buttonMarkup.querySelector("button"),
-          link: stepEl.querySelector("a[href]"),
-          summary: stepEl.querySelector(".step__summary"),
+          button: buttonMarkup.querySelector('button'),
+          link: stepEl.querySelector('a[href]'),
+          summary: stepEl.querySelector('.step__summary'),
           title: stepTitleEl.textContent.trim(),
         };
 
@@ -189,9 +189,9 @@
 
         // Add button id attribute, add button event listener, passing current
         // index to handler.
-        step.button.setAttribute("aria-controls", stepId);
+        step.button.setAttribute('aria-controls', stepId);
         step.button.addEventListener(
-          "click",
+          'click',
           handleStepButtonClick.bind(null, index),
         );
 
@@ -214,11 +214,11 @@
         return;
       }
 
-      const stepControlTemplate = document.createElement("template");
-      stepControlTemplate.innerHTML = Drupal.theme("controlButtonHtml");
+      const stepControlTemplate = document.createElement('template');
+      stepControlTemplate.innerHTML = Drupal.theme('controlButtonHtml');
       const stepControlMarkup = stepControlTemplate.content.cloneNode(true);
-      const stepControlButton = stepControlMarkup.querySelector("button");
-      const stepControlIcon = stepControlMarkup.querySelector("i.fas");
+      const stepControlButton = stepControlMarkup.querySelector('button');
+      const stepControlIcon = stepControlMarkup.querySelector('i.fas');
 
       // Insert button into DOM.
       stepListEl.parentElement.prepend(stepControlMarkup);
@@ -227,10 +227,10 @@
       toggleControlButton(false);
 
       // Add aria-controls attribute.
-      stepControlButton.setAttribute("aria-controls", stepIds.join(" "));
+      stepControlButton.setAttribute('aria-controls', stepIds.join(' '));
 
       // Add button event listener.
-      stepControlButton.addEventListener("click", handleControlButtonClick);
+      stepControlButton.addEventListener('click', handleControlButtonClick);
     },
   };
 })(Drupal);
